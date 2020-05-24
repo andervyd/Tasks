9df515475312662cf53335e5b520a9bb62e6e97f
andervyd <andervyd@rambler.ru>
2020-05-24 07:23:31 +0300

    add annotatios for class checkPalindrome

--- src/task_palindrome/Answer.java
+++ src/task_palindrome/Answer.java
@@ -1,5 +1,12 @@
 package task_palindrome;
 
+/**
+   A palindrome is a word, number, phrase, or other sequence of characters which reads the same backward as forward,
+   such as madam, racecar. There are also numeric palindromes, including date/time stamps using short digits 11/11/11 11:11
+   and long digits 02/02/2020. Sentence-length palindromes may be written when allowances are made for adjustments to capital letters,
+   punctuation, and word dividers, such as "A man, a plan, a canal, Panama!".
+*/
+
 public class Answer {
     public static void checkPalindrome(StringBuilder inWord) {
         StringBuilder revWord = new StringBuilder(inWord);
