fcfb7a8547b248d8fbfaaf049f60b86bea101680
andervyd <andervyd@rambler.ru>
2020-05-24 13:05:47 +0300

    change code, add clear 'space' char and symbol

--- src/task_palindrome/Answer.java
+++ src/task_palindrome/Answer.java
@@ -8,19 +8,37 @@ package task_palindrome;
 */
 
 public class Answer {
-    public static void checkPalindrome(StringBuilder inWord) {
-        StringBuilder revWord = new StringBuilder(inWord);
-        String strInWord = inWord.toString();
-        String strRevWord = revWord.toString();
 
-        if(strInWord.toUpperCase().equals(strRevWord.toUpperCase())) {
-            System.out.print("Word " + inWord + " - palindrome");
-        } else {
-            System.out.print("Word " + inWord + " - not palindrome");
+    public static void checkPalindrome(String inWord) {
+        String outWord = new String(inWord);
+        String symbol = ".,?!()/";
+
+        for(char delSymbol : symbol.toCharArray()) {
+            inWord = inWord.replace(delSymbol, ' ');
         }
-    }
+
+        String temp = inWord.replace(" ", "").toUpperCase();
+
+        int startLine = 0;
+        int endLine = temp.length() - 1;
+        boolean check = true;
+        while(startLine < endLine) {
+            if(temp.charAt(startLine) != temp.charAt(endLine)) {
+                check = false;
+                break;
+            } else {
+                startLine++;
+                endLine--;
+            }
+        }
+        if(!check) {
+            System.out.println("Phrase: " + outWord + " - not palindrome");
+       } else System.out.println("Phrase: " + outWord + " - palindrome");
+}
+
 
     public static void main(String[] args) {
-        checkPalindrome(new StringBuilder("abBA"));
+        new Answer();
+        Answer.checkPalindrome("A man, a plan, a canal, Panama!");
     }
 }
