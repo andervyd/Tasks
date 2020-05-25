72320155375c067670cfd2797da4fa4cf5fb3341
andervyd <andervyd@rambler.ru>
2020-05-25 12:48:36 +0300

    add new task: 99 bottle of beer

---
+++ src/task_99_Bottles_of_Beer/Answer.java
@@ -0,0 +1,19 @@
+package task_99_Bottles_of_Beer;
+
+/**
+ *  99 bottles of beer on the wall, -------- int
+ *  99 bottles of beer.
+ *  Take one down, pass it around,
+ *  98 bottles of beer on the wall...
+ *  ...
+ *  No more bottles of beer on the wall,
+ *  no more bottles of beer.
+ *  Go to the store and buy some more,
+ *  99 bottles of beer on the wall...  ----- repeated
+ */
+
+public class Answer {
+    public static void main(String[] args) {
+
+    }
+}
